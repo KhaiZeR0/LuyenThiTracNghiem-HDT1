35d47ac32b15860036eda2f1dc89dbc4ed6598e6
khaizr0 <caophankhai123@gmail.com>
2023-12-04 11:19:02 +0700

    bhgjn

--- Final - OOP/Program.cs
+++ Final - OOP/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Final___OOP.winform;
+using Final___OOP.winform.student;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +18,7 @@ namespace Final___OOP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu_student());
         }
     }
 }
